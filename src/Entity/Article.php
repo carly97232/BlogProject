2285a9ef5e9db401b2ee00a48150f127ce08c4b3
Jovanela Daribo <carly972@live.fr>
2018-11-18 22:38:17 +0100

    Premier essai non concluant de la quête 8

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -31,6 +31,12 @@ class Article
      */
     private $content;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -68,6 +74,18 @@ class Article
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
